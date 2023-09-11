7ad2eeb6b4b35fceb896bb914939a48b844db127
mukhlisa96 <mukhlisakhusanboyeva@gmail.com>
2023-09-11 19:38:33 +0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react"
+import Category from "./components/Category"
+import Food from "./components/Food"
+import Footer from "./components/Footer"
+import HeadlineCards from "./components/HeadlineCards"
+import Hero from "./components/Hero"
+import Navbar from "./components/Navbar"
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar />
+      <Hero />
+      <HeadlineCards />
+      <Food />
+      <Category />
+      <Footer />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
---
+++ src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Footer = () => {
+    return (
+        <div className="max-w-[1640px] m-auto px-4 py-12 bg-black text-gray-50 justify-center items-center">
+            Best <strong>Eats</strong> - Food Delivery
+        </div>
+    )
+}
+
+export default Footer
